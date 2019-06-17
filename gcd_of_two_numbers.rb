a8fadbb0eab74382ea00817cd3694876febeb713
kattak2k <kkiran.ror@gmail.com>
2019-06-16 23:59:34 -0400

    Program to find GCD or HCF of two numbers

---
+++ gcd_of_two_numbers.rb
@@ -0,0 +1,25 @@
+# Program to find GCD or HCF of two numbers
+# 98 - 56 => # 42
+# 56 - 42 => # 14
+# 42 - 14 => # 28
+# 28 - 14 => # 14
+
+def gcd(a, b)
+  # Everything divides 0
+  return b if a == 0
+
+  return a if b == 0
+
+  # base case
+  return a if a == b
+
+  # a is greater
+  return gcd(a - b, b) if a > b
+
+  gcd(a, b - a)
+end
+
+# Driver program to test above function
+a = 98
+b = 56
+p gcd(a, b)
