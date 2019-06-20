5c3b24bdc5fd026579cd4a08948e1065223f8e87
kattak2k <kkiran.ror@gmail.com>
2019-06-20 18:01:57 -0400

    updated format

--- matching_element_in_array.js
+++ matching_element_in_array.js
@@ -5,25 +5,46 @@ function filteredArray(arr, elem) {
     let newArr = [];
     // change code below this line
     let len = arr.length;
-    for (let i = 0; i < len; i++ ){
+    for (let i = 0; i < len; i++) {
         //check if the element in present in 2D array 
-         if (arr[i].indexOf(elem) < 0) {  
-             // push the array into new array if the value doesn't exists 
-              newArr.push(arr[i]);
-         }; 
+        if (arr[i].indexOf(elem) < 0) {
+            // push the array into new array if the value doesn't exists 
+            newArr.push(arr[i]);
+        };
     };
-  
+
     // change code above this line
     return newArr;
-  }
-  
-  // change code here to test different cases:
-  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
-  console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18)); 
-  //should return [ [10, 8, 3], [14, 6, 23] ]
-  console.log(filteredArray([ ["trumpets", 2], ["flutes", 4], ["saxophones", 2] ], 2));
+}
+
+// change code here to test different cases:
+console.log(filteredArray([
+    [3, 2, 3],
+    [1, 6, 3],
+    [3, 13, 26],
+    [19, 3, 9]
+], 3));
+console.log(filteredArray([
+    [10, 8, 3],
+    [14, 6, 23],
+    [3, 18, 6]
+], 18));
+//should return [ [10, 8, 3], [14, 6, 23] ]
+console.log(filteredArray([
+    ["trumpets", 2],
+    ["flutes", 4],
+    ["saxophones", 2]
+], 2));
 //should return [ ["flutes", 4] ]
-   console.log(filteredArray([ ["amy", "beth", "sam"], ["dave", "sean", "peter"] ], "peter"));
+console.log(filteredArray([
+    ["amy", "beth", "sam"],
+    ["dave", "sean", "peter"]
+], "peter"));
 //should return [ ["amy", "beth", "sam"] ]
-   console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([
+    [3, 2, 3],
+    [1, 6, 3],
+    [3, 13, 26],
+    [19, 3, 9]
+], 3));
 //should return [ ]
