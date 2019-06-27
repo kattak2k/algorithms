cf38711f1490ace8ba1ba083209adfde89ade78b
kattak2k <kkiran.ror@gmail.com>
2019-06-27 18:21:24 -0400

    added outputs

--- caesar_cipher_encoder.rb
+++ caesar_cipher_encoder.rb
@@ -23,3 +23,9 @@ end
 # p rot13('SERR CVMMN!', 13)
 # p rot13('SERR YBIR?', 13)
 p rot13('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.', 13)
+
+# outputs -->
+# rot13("SERR PBQR PNZC") should decode to FREE CODE CAMP
+# rot13("SERR CVMMN!") should decode to FREE PIZZA!
+# rot13("SERR YBIR?") should decode to FREE LOVE?
+# rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
