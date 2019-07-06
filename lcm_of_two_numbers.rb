a7afba004b26e4eee6717b5ca2c9c5f14089f42d
kattak2k <kkiran.ror@gmail.com>
2019-07-06 12:46:24 -0400

    LCM of two numbers

---
+++ lcm_of_two_numbers.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Recursive function to return gcd of a and b
+def gcd(a, b)
+  return b if a == 0
+
+  gcd(b % a, a)
+ end
+
+# Function to return LCM of two numbers
+def lcm(a, b)
+  (a * b) / gcd(a, b)
+end
+
+# Driver program to test above function
+#p lcm(15, 20)
+p lcm(98, 56)
+
+# This code is contributed by Danish Raza
