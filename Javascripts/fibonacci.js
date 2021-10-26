68cb567fc3ea7d603aae5edc5a35a461d8d87d0c
Ravi Katta <kkiran.ror@gmail.com>
2021-10-25 22:29:14 -0400

    fib js upgrade

--- Javascripts/fibonacci.js
+++ Javascripts/fibonacci.js
@@ -30,7 +30,7 @@ function memoize(fn) {
     return fib(n - 1) + fib(n - 2);
   }
   
-  const fib = memoize(slowFib);
+const fib = memoize(slowFib);
   
 console.log(fib(10))
   
@@ -46,3 +46,16 @@ console.log(fib(10))
   //
   //   return result[n];
   // }
+
+
+ // Solution 2
+
+ const fib = (n,memo={}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+   
+    memo[n] = fib(n-1,memo) + fib(n-1,memo);
+    return memo[n];
+ }
+
+ // O(2n)  = O(n) space & time
